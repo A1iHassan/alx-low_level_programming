1f3770d88fb4c201ce73cbd67a223e26036ce3c7
A1iHassan <ali012wkout@gmail.com>
2023-09-07 14:32:31 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main()
 {
 	const char str = '"';
-	const char *str2 = "Programming is like building a multilingual puzzle";
+	const char str2[100] = "Programming is like building a multilingual puzzle";
 	puts(str);
 	puts(str2);
 	return 0;
