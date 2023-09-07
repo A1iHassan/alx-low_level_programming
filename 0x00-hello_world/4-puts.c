4e699a30db12474b6676fa19f6d05de701714e55
A1iHassan <ali012wkout@gmail.com>
2023-09-07 14:30:56 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	const char *str = '"';
+	const char str = '"';
 	const char *str2 = "Programming is like building a multilingual puzzle";
 	puts(str);
 	puts(str2);
