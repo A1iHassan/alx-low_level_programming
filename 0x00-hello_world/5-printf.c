243badb3187645fa45bb364202e2578dbd4b11eb
A1iHassan <ali012wkout@gmail.com>
2023-09-07 15:15:02 -0700

    task 5 answer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: bla bla
+ *
+ * Return: bla bla
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return(0);
+}
