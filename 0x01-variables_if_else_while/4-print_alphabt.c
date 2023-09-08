754349fad0e9df85943be0534531f1fd1a3a2a66
A1iHassan <ali012wkout@gmail.com>
2023-09-08 07:22:12 -0700

    task 4 answer

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Describtion: bla bla
+ * Return: bla bla
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'e' || x == 'q')
+			continue;
+		putchar(x);
+		++x;
+	}
+	putchar('\n');
+	return (0);
+}
