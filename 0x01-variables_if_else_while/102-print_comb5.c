0109bd33b92e87f4a44c83f7eb94276c6f6d4112
A1iHassan <ali012wkout@gmail.com>
2023-09-08 12:11:02 -0700

    task 12 answer

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,4 +15,27 @@ int main(void)
 
 	for (a = 48; a < 58; a++)
 	{
-
+		for (b = 48; b < 57; b++)
+		{
+			for (c = a; c < 58; c++)
+			{
+				for (d = b + 1; d < 58; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' '');
+					putchar(c);
+					putchar(d);
+					if (a == c == d == 57 && b == 56)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
