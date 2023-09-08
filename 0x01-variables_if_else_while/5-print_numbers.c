c544f5b0c97db7f91d4a05c263a7cbf630ab52a8
A1iHassan <ali012wkout@gmail.com>
2023-09-08 07:30:04 -0700

    task 5 answer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Describtion: bla bla
+ * Return: bla bla
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
