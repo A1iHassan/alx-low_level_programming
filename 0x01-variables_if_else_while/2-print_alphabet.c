ccfc0cb4b11bc528893a61f8530bf3a925c43835
A1iHassan <ali012wkout@gmail.com>
2023-09-08 06:56:05 -0700

    edit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,10 @@
 
 int main(void)
 {
-	char *s = "\n";
 	int i;
 	char arr[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	for (i = 0; i < 26; i++)
 		putchar(arr[i]);
-	putchar(s);
 	return (0);
 }
