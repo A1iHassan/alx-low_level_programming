b74d02bf8621f156259d78e67ab94774e9fc4901
A1iHassan <ali012wkout@gmail.com>
2023-09-08 07:48:42 -0700

    task 6 answer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Describtion: bla bla
+ * Return: baaaaaka
+ */
+
+int main(void)
+{
+	char x = '0';
+
+	while (x < '10')
+	{
+		putchar(x);
+		++x;
+	}
+	putchar('\n');
+	return (0);
