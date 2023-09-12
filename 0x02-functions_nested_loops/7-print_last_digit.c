9b4a65021638c36c958edd67d95a422c3b303183
A1iHassan <ali012wkout@gmail.com>
2023-09-12 14:07:29 -0700

    edit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,13 @@
 
 int print_last_digit(int x)
 {
-	int y = x % 10;
-	char z = (char)y;
-	_putchar(z);
+	int y;
+
+	if (x < 0)
+		y = -1 * (x % 10);
+	else
+		y = x % 10
+
+	_putchar(y + '0');
 	return (y);
 }
