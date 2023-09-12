b3b3a5d4ca1699cae13d228639e88ec3b9e3e2fa
A1iHassan <ali012wkout@gmail.com>
2023-09-12 12:42:36 -0700

    task 4 answer

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry
+ * int c - bla bla
+ * Describtion: bla bla
+ * Return: bla bla
+ */
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
