5b96a1bb2d71bba1edab960a793f2403fadcdb68
A1iHassan <ali012wkout@gmail.com>
2023-09-12 14:43:38 -0700

    edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
-  1 #include "main.h"
-  2 
-  3 /**
-  4  * jack_bauer - bla
-  5  * Describtion: bla
-  6  * Return: bla
-  7  */
-  8 
-  9 void jack_bauer(void)
- 10 {
- 34 }
+#include "main.h"
+
+/**
+ * jack_bauer - bla
+ * Describtion: bla
+ *Return: bla
+ */
+
+void jack_bauer(void)
+{
+}
