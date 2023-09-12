feeb26125d6dfeabd24d8e669d6226b27b037608
A1iHassan <ali012wkout@gmail.com>
2023-09-12 12:13:57 -0700

    task 1 answer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * Describtion: bla bla
+ * Return: bla bla
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	return;
+}
