cd54b4e96758181782e82a0225045b793703b61a
A1iHassan <ali012wkout@gmail.com>
2023-09-14 15:33:44 -0700

    -

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - bla
+ */
+
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 49; i++)
+	{
+		for (j = 0; j <= 57; j++)
+		{
+			if (i == 48)
+				k = j;
+			else
+				k = i;
+			
+			_putchar(k);
+
+			if (k = 1 && (j < 48 && j < 53))
+				_putchar(j);
+		}
+	}
+}
