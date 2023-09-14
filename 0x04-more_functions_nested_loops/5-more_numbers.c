3453a62d8961506a8116317000509335440b6a5c
A1iHassan <ali012wkout@gmail.com>
2023-09-14 15:40:07 -0700

    -

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,25 @@
 
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
-	for (i == 48; i <= 49; i++)
+	for (l == 0; l <= 10; l++)
 	{
-		for (j == 0; j <= 57; j++)
+		for (i == 48; i <= 49; i++)
 		{
-			if (i == 48)
-				k = j;
-			else
-				k = i;
-			
-			_putchar(k);
+			for (j == 0; j <= 57; j++)
+			{
+				if (i == 48)
+					k = j;
+				else
+					k = i;
 
-			if (k == 1 && (j < 48 && j < 53))
-				_putchar(j);
+				_putchar(k);
+
+				if (k == 1 && (j < 48 && j < 53))
+					_putchar(j);
+			}
 		}
+		_putchar('\n');
 	}
 }
