aaa7005974d5e4c137c22c65ef0f69f96b5629fb
A1iHassan <ali012wkout@gmail.com>
2023-09-14 16:19:14 -0700

    -

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,24 +9,18 @@ void more_numbers(void)
 	int i;
 	int j;
 	int k;
-	int l;
 
-	for (l = 0; l <= 10; l++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 48; i <= 49; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			for (j = 0; j <= 57; j++)
+			k = j;
+			if (j > 9)
 			{
-				if (i == 48)
-					k = j;
-				else
-					k = i;
-
-				_putchar(k);
-
-				if (k == 1 && (j < 48 && j < 53))
-					_putchar(j);
+				_putchar(49);
+				k = j % 10;
 			}
+			_putchar(k + 48);
 		}
 		_putchar('\n');
 	}
