15327bbe3f837d6f8498f9394c2f2b5e41ad6c6e
A1iHassan <ali012wkout@gmail.com>
2023-09-14 17:28:13 -0700

    -

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - bla
+ * Describtion: bla
+ * Return: bla
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 = 0 && i % 5 = 0)
+			print("FizzBuzz");
+		else if (i % 3 = 0)
+			print("Fizz");
+		else if (i % 5 = 0)
+			print("Buzz");
+		else
+			print("%d", i);
+		
+		if (i != 100)
+			print(" ");
+	}
+	print("\n");
+}
