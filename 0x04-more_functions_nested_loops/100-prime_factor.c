fd3571d78aede25ca63fbde2018a15db55eada14
A1iHassan <ali012wkout@gmail.com>
2023-09-15 07:21:07 -0700

    -

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,24 +6,38 @@
  * Return: bla
  */
 
-int main(void)
+long long findLargestPrimeFactor(long long n)
 {
-	int i;
-	int count = 0;
-	unsigned long long int num = 612852475143;
+	long long largestPrime = -1;
 
-	for (i = 2; i < 700000000000; i++)
+	while (n % 2 == 0)
 	{
-		if (num % i == 0)
+		largestPrime = 2;
+		n /= 2;
+	}
+
+	for (long long i = 3; i * i <= n; i += 2)
+	{
+		while (n % i == 0)
 		{
-			printf("%d\n", i);
-			num /= i;
-			count++;
-		}
-		if (count == 4)
-		{
-			break;
+			largestPrime = i;
+			n /= i;
 		}
 	}
+
+	if (n > 2)
+	{
+		largestPrime = n;
+	}
+	return (largestPrime);
+}
+
+int main(void)
+{
+	long long number = 612852475143;
+	long long largestPrime = findLargestPrimeFactor(number);
+
+	printf("%lld\n", largestPrime);
+
 	return (0);
 }
