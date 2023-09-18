d9d20bca8a0e32683e888e0fe46324b0741491d2
A1iHassan <ali012wkout@gmail.com>
2023-09-18 12:59:08 -0700

    =

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - bla
+ * @str: bla
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	while (j <= i)
+	{
+		if (j % 2 = 0)
+			_putchar(str[j]);
+		j++;
+	}
+}
