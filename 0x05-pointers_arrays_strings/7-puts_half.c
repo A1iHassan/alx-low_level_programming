ab98177233092d8544433e255d3dc273acfc9eb0
A1iHassan <ali012wkout@gmail.com>
2023-09-18 16:49:37 -0700

    -

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,17 @@
 
 void puts_half(char *str)
 {
-	if (str == NULL) {
-		return;
-	}
+	int length;
+	int start;
+	int i;
 
-	int length = 0;
+	length = 0;
 
 	while (str[length] != '\0') {
 		length++;
 	}
 
-	int start;
+	start;
 
 	if (length % 2 == 0) {
 		start = length / 2;
@@ -25,7 +25,7 @@ void puts_half(char *str)
 		start = (length - 1) / 2;
 	}
 
-	for (int i = start; i < length; i++) {
+	for (i = start; i < length; i++) {
 		_putchar(str[i]);
 	}
 
