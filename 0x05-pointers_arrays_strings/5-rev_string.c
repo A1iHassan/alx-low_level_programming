681c5ccb7c37eea222cb1ed99958eae7492f3643
A1iHassan <ali012wkout@gmail.com>
2023-09-18 12:34:35 -0700

    =

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - bla
+ * @s: bla
+ */
+
+void rev_string(char *s)
+{
+	char *rev;
+	int i = 0;
+	int j;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	j = i;
+
+	while (i--)
+	{
+		rev[j - i] = s[i];
+	}
+	s = rev;
+}
