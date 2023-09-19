5fb3837750a6ba74e2b9df0541fb3facc08a9b92
A1iHassan <ali012wkout@gmail.com>
2023-09-18 17:33:53 -0700

    -

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
-		if (src[i] == '\0')
-			break;
+		i++;
 	}
 	return (dest);
 }
