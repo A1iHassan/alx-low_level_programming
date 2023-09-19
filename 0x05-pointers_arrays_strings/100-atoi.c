d0eddc92eff7fb242d2412e61ab017917402570e
A1iHassan <ali012wkout@gmail.com>
2023-09-18 17:55:21 -0700

    -

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - bla
+ * @s: bla
+ * Return: bla
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int result;
+	bool sign = true;
+	char *num;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign = !sign;
+		else if (s[i] >= 48 && s[i] <= 57)
+		{
+			num[j] = s[i];
+			j++;
+		} else {
+			return (0);
+		}
+	}
+
+	result = (int)*num;
+
+	if (sign == true)
+		result = -result;
+	return (result);
+}
