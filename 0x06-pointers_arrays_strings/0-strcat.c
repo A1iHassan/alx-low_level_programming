5d529de608b7653a18bee537e5ba7185f5c6cf94
A1iHassan <ali012wkout@gmail.com>
2023-09-20 03:48:27 -0700

    task 0 answer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strcat - bla
+ * @dest: bla
+ * @src: bla
+ * Return: bla
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *dest;
+	char *src;
+	char *temp;
+	int i, j, k, l, m;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	m = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	k = i + j;
+
+	temo = (char*)malloc(k * sizeof(char));
+
+	while (l < k)
+	{
+		temp[l] = dest[l];
+
+		if (l == i)
+		{
+			while (m <= l)
+			{
+				temp[l] = src[m];
+				m++;
+				l++;
+			}
+		}
+		l++;
+	}
+
+	dest = temp;
+	return (dest);
+}
