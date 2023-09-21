13cd81100fcffa2805a27a2aab7085ffee034e56
A1iHassan <ali012wkout@gmail.com>
2023-09-21 11:16:39 -0700

    task 4 answer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - bla
+ * @a: bla
+ * @n: bla
+ */
+
+void reverse_array(int *a, int n)
+{
+	char x;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		a[i] = a[n-i];
+	}
+}
