6489ad40a9cd00b99f2c758ef9dd90e11165f6d7
A1iHassan <ali012wkout@gmail.com>
2023-09-21 12:15:53 -0700

    -

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,12 @@
 
 char *string_toupper(char *x)
 {
-	char *y;
 	int i = sizeof(x) / sizeof(char);
 
-	while ( i > 0)
+	while (i > 0)
 	{
 		if (x[i] > 96 && x[i] < 122)
 			x[i] -= 32;
 	}
-	y = x;
-	return (y);
+	return (x);
 }
