ff9511cf109288f0cdc128d16e475ea5153bd6e5
A1iHassan <ali012wkout@gmail.com>
2023-09-21 10:32:53 -0700

    =

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,21 @@
 int _strcmp(char *s1, char *s2);
 {
 	int i = 0;
+	int j;
 
-	while (i != 0)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 > *s2)
+		if (s1[i] > s2[i])
 		{
-			i = 1;
+			j = 1;
 			break;
 		}
-		if (*s1 < *s2)
+		if (s1[i] < s2[i])
 		{
-			i = -1;
+			j = -1;
 			break;
 		}
-		s1++;
-		s2++;
+		i++;
 	}
 	return (i);
 }
