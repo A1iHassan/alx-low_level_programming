6daa56ab2d17a90039f98b4da106ab37f01b8532
A1iHassan <ali012wkout@gmail.com>
2023-09-25 12:28:47 -0700

    task 3 redo

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - bla
+ * @s: bla
+ * @accept: bla
+ * Return: bla
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (*accept >= '\0')
+	{
+		while (*s >= '\0')
+		{
+			if (*s == *accept)
+				i++;
+			s++;
+		}
+		accept++;
+	}
+	return (i);
+}
