c8b8ae747f5a183205d3e05d288ffe495b649584
A1iHassan <ali012wkout@gmail.com>
2023-09-28 14:28:14 -0700

    task 6 answer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a given nummber
+ * @n: a given number
+ * Return: a value of type int
+ */
+
+int _sqrt_recursion(int n)
+{
+	int avr, div;
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if the input is a prime integer
+ * @n: input number
+ * Return: a value of the type int
+ */
+
+int is_prime_number(int n)
+{
+	if ((n % 2 != 0) && (n & 3 != 0) && (n % 5 != 0))
+		return (1);
+	return (0);
+}
