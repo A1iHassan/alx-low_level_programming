72843d303831364c2fd76168e714605d9dc5921c
A1iHassan <ali012wkout@gmail.com>
2023-10-02 16:23:59 -0700

    -

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - writes the name of the program
+ * @argc: number of given arguments
+ * @argv: given arguments vector
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
 	(void)argc;
