446b05765b5ce9b03508facc55375378fbdd00f4
A1iHassan <ali012wkout@gmail.com>
2023-10-03 09:41:44 -0700

    -

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 
 /**
+ * change - a recursive * operation
+ * @x: the given number
+ * Return: a value of type int
+ */
+
+int change(int x)
+{
+	int sum = 0;
+
+	if (x / 25 > 0)
+		return (x / 25 + change(x % 25));
+	else if (x / 10 > 0)
+		return (x / 10 + change(x % 10));
+	else if (x / 5 > 0)
+		return (x / 5 + change(x % 5));
+	else if (x / 2 > 0)
+		return (x / 2 + change(x % 2));
+	else if (x / 1 > 0)
+		return (x / 1);
+}
+
+/**
  * main - calculates the amount of change
  * @argc: number of given arguments
  * @argv: arguments vector
@@ -10,10 +32,7 @@
 
 int main(int argc, char **argv)
 {
-	int i, j, sum;
-
-	(void)sum;
-	(void)j;
+	int i, j; 
 
 	if (argc > 2)
 	{
@@ -21,7 +40,12 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	i = atoi(argv[1]);
-	if (i < 0)
+	if (i <= 0)
+	{
 		printf("0\n");
+		return (0);
+	}
+	j = change(i);
+	printf("%d\n", j);
 	return (0);
 }
