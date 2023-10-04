3e7e9b88a7038a0963bf44ffee5586b0bf40c336
A1iHassan <ali012wkout@gmail.com>
2023-10-04 06:49:36 -0700

    -

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,9 @@ char *create_array(unsigned int size, char c)
 		return (0);
 	a = malloc(sizeof(char) * size);
 
+	if (a == NULL)
+		return (0);
+
 	while (size--)
 	{
 		a[size] = c;
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main"
+
+/**
+ * _strdup - copies a given string to a newly allocated memory
+ * @str: the given string
+ * Return: a value of type char *
+ */
+
+char *_strdup(char *str)
+{
+
