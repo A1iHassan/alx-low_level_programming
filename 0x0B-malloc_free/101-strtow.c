aa690ef0b2b2b2062782965bd2773c04c0fea14b
A1iHassan <ali012wkout@gmail.com>
2023-10-04 16:14:36 -0700

    -

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,35 +2,6 @@
 #include <stdio.h>
 
 /**
- * _strcmp - compares two strings
- * @s1: s1
- * @s2: s2
- * Return: 1 - 0
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] > s2[i])
-		{
-			j = 1;
-			break;
-		}
-		if (s1[i] < s2[i])
-		{
-			j = -1;
-			break;
-		}
-		i++;
-	}
-	return (j);
-}
-
-/**
  * strtow - splits a given string into its words
  * @str: a given string
  * Return: a value of type char **
@@ -40,7 +11,7 @@ char **strtow(char *str)
 {
 	int l, i = 0, k = 0, m = 0, n = 1;
 	char **a;
-	if (_strcmp(str, "") || _strcmp(str, " "))
+	if (str == NULL || str == 0)
 		return (NULL);
 	while (*str != '\0')
 	{
