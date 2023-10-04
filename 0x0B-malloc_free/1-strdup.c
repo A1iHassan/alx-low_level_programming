912d67ecec1a2027668238bade62c7943675d4c5
A1iHassan <ali012wkout@gmail.com>
2023-10-04 07:47:37 -0700

    -

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,21 +9,23 @@
 
 char *_strdup(char *str)
 {
+	int i = 0;
 	char *a;
 
 	if (str == NULL)
 		return (NULL);
 
-	a = malloc(sizeof(str));
+	a = malloc(sizeof(str) - 1);
+	printf("%ld\n", sizeof(str));
 
 	if (a == NULL)
 		return (0);
 
 	while (*str)
 	{
-		*a = *str;
+		a[i] = *str;
 		printf("%c\n", *a);
-		a++;
+		i++;
 		str++;
 	}
 	return (a);
