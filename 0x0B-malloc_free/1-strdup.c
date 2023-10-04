583f6ad4b56578f1de104828d49288abd8769add
A1iHassan <ali012wkout@gmail.com>
2023-10-04 07:02:05 -0700

    task 1 answer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include "main"
 
 /**
  * _strdup - copies a given string to a newly allocated memory
@@ -9,4 +8,25 @@
 
 char *_strdup(char *str)
 {
+	int i = 0;
+	int j = 0;
+	char *a;
 
+	if (str == NULL)
+		return (NULL);
+
+	while(*str)
+		i++;
+
+	a = malloc(i * sizeof(char) + 1);
+
+	if (a == NULL)
+		return (0);
+
+	while(j <= i)
+	{
+		a[j] = str[j];
+		j++;
+	}
+	return (a);
+}
