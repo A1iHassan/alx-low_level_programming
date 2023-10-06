24f6aec05e00276e059f7b179c2d3bb630bb27d4
A1iHassan <ali012wkout@gmail.com>
2023-10-06 07:00:06 -0700

    task 0 answer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates a given number of bytes on the heap
+ * @b: number of bytes
+ * Return: a value of type void *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+	
+	a = malloc(b);
+	if (a == NULL)
+		return (98);
+	return (a);
+}
