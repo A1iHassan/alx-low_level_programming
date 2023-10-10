0424441d8a507d172a4a2d87f5257b52ab21decc
A1iHassan <ali012wkout@gmail.com>
2023-10-10 13:30:13 -0700

    -

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - defines dog
+ * @name: first member, holds the dog name
+ * @owner: second member, holds the dog's owner
+ * @age: third member, holds the dog's age
+ * Describtion: a data type of dog that holds important informations about the dog
+ */
+
 struct dog {
 	char *name;
 	char *owner;
