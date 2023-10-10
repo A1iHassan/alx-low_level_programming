2a545e66bcfaf98f2f2088fc5b2e5fb48def9695
A1iHassan <ali012wkout@gmail.com>
2023-10-10 15:43:52 -0700

    -

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -14,13 +15,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t aa;
 	dog_t *a;
-	char *dname, *downer;
 
 	a = &aa;
 
-	a->name = name;
+	a->name = strdup(name);
 	a->age = age;
-	a->owner = owner;
+	a->owner = strdup(owner);
 
 	return (a);
 }
