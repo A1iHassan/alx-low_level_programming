e8ea5a05f85ac48ebb09d0d1cd315cda325e3964
A1iHassan <ali012wkout@gmail.com>
2023-10-10 15:31:57 -0700

    -

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,17 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void d_dup(char *a, char *b)
-{
-	int i = 0;
-	while (*b)
-	{
-		a[i] = *b;
-		i++;
-		b++;
-	}
-}
-
 /**
  * new_dog - creates a new dog
  * @name: dog name
@@ -25,11 +14,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t aa;
 	dog_t *a;
-	char dname[100], downer[100];
 
 	a = &aa;
-	d_dup(dname, name);
-	d_dup(downer, owner);
 
 	a->name = dname;
 	a->age = age;
