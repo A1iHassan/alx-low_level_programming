0d3f51b5354ce926bc4794807ea98b2c6697c9b3
A1iHassan <ali012wkout@gmail.com>
2023-10-10 13:33:10 -0700

    =

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: first member, holds the dog name
  * @owner: second member, holds the dog's owner
  * @age: third member, holds the dog's age
- * Describtion: a data type of dog that holds important informations about the dog
+ * Describtion: a data type of dog that holds important informations
  */
 
 struct dog
