b76ca03566b9a4615f2a4a1aa572819f24c53103
A1iHassan <ali012wkout@gmail.com>
2023-10-10 11:43:06 -0700

    -

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3_-FUNCTION_LIKE_MACRO_H
-#define 3_-FUNCTION_LIKE_MACRO_H
+#ifndef 3_-_FUNCTION_LIKE_MACRO_H
+#define 3_-_FUNCTION_LIKE_MACRO_H
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
