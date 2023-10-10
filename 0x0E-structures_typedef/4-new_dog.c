dc8d438e232e97cd3e40e46c329fa3972ee786db
A1iHassan <ali012wkout@gmail.com>
2023-10-10 16:04:02 -0700

    -

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	a = &aa;
 
-	a->name = strcpy(a->name, name);
+	a->name = name;
 	a->age = age;
-	a->owner = strcpy(a->owner, owner);
+	a->owner = owner;
 
 	if (a->name == NULL || age == 0 || a->owner == NULL)
 		return (NULL);
