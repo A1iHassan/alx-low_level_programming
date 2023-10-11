789494d8063eb821eafa7491279a801e3b0a67bd
A1iHassan <ali012wkout@gmail.com>
2023-10-11 08:39:33 -0700

    task 3 answer

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char **argv)
+{
+	int i, j, k;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[3] == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	k = (*get_op_func(argv[2]))(i, j);
+	printf("%d\n", k);
+	return (0);
+}
