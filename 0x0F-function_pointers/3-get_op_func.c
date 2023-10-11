697b59ded410d8eee785116f073fdb4380485794
A1iHassan <ali012wkout@gmail.com>
2023-10-11 09:46:51 -0700

    -

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - selects the suitable function
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (ops[i].op[0] == *s)
+		if (ops[i].op[0] == *s && strlen(s) == 1)
 		{
 			return (ops[i].f);
 		}
