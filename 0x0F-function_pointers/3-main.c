83962460b80b69ea38c022a26d34e7b440d5566b
A1iHassan <ali012wkout@gmail.com>
2023-10-11 10:10:20 -0700

    -

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 	(*argv[2] != '/') &&
 	(*argv[2] != '*') &&
 	(*argv[2] != '%') &&
-	(strlen(argv[2] != 1)))
+	(strlen(argv[2]) != 1))
 	{
 		printf("Error\n");
 		exit(99);
