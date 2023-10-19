9c1910fabb1a2f52a998a806d0ea62dc13368300
A1iHassan <ali012wkout@gmail.com>
2023-10-19 15:28:07 -0700

    -

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - calculates a list's length
+ * @h: head pointer
+ * Return: list's length
+ */
+
 size_t list_len(const list_t *h)
 {
 	if (h == NULL)
