e2dd8a7cbc6cea6ad3d2342518dedea484ffbede
A1iHassan <ali012wkout@gmail.com>
2023-10-19 16:11:02 -0700

    -

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *new_node;
+
+	while(*head == NULL)
+	{
+		temp = *head;
+		head = temp->next;
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	temp->next = new_node;
+
+	return (new_node);
+}
