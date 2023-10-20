7c8ef855b8ba796e1404b18cc89025538b4b9ea5
A1iHassan <ali012wkout@gmail.com>
2023-10-20 12:21:02 -0700

    -

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,14 +11,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	temp = *head;
-	while(temp->next != NULL)
-		temp = temp->next;
-
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
-	temp->next = new_node;
 
-	return (new_node);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	} else {
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return new_node;
 }
