005a3b3693af8b59ae4396cbacba065e89e9bdc1
A1iHassan <ali012wkout@gmail.com>
2023-10-20 12:39:31 -0700

    -

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a statement
+ * Return: a value of type int
+ */
+
+int main(void)
+{
+	char *a = "You're beat! and yet, you must allow,\n";
+
+	printf("%sI bore my house upon my back!\n", a);
+	main();
+	return (0);
+}
+
