04b09ac6cd08e09e3e553c5b6fefd04b345244af
A1iHassan <ali012wkout@gmail.com>
2023-10-24 08:54:54 -0700

    -

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -80,6 +80,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	temp->next = cursor_after;
-	cursor_before = temp;
+	cursor_before->next = temp;
 	return (temp);
 }
