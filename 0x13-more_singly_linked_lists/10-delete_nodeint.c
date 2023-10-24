c17f0bcdb447e05d0d70aaf042288839694ead7e
A1iHassan <ali012wkout@gmail.com>
2023-10-24 09:32:59 -0700

    :-:

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts list nodes
+ * @h: list head
+ * Return: nodes count
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	int i = 0;
+
+	while (cursor)
+	{
+		i++;
+		cursor = cursor->next;
+	}
+	return (i);
+}
+
+/**
+ * get_nodeint_at_index - searches a list by index
+ * @head: list head
+ * @index: position of desired node
+ * Return: desired node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = (unsigned int)listint_len(head);
+	int j;
+	listint_t *cursor;
+
+	if (!head || i < index)
+		return (NULL);
+
+	cursor = head;
+	for (j = index; j > 0; j--)
+		cursor = cursor->next;
+	return (cursor);
+}
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = listint_len(*head);
+
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -52,7 +52,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = (unsigned int)listint_len(*head);
-	listint_t *cursor_after, *cursor_before, *temp;
+	listint_t *cursor, *temp;
 
 	if ((!*head && i != 0) || i < idx)
 		return (NULL);
@@ -72,20 +72,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (temp);
 	}
 
-	cursor_after = get_nodeint_at_index(*head, idx);
-	if (!cursor_after)
+	cursor = get_nodeint_at_index(*head, idx - 1);
+	if (!cursor)
 	{
-		free(cursor_after);
-		return (NULL);
-	}
-	cursor_before = get_nodeint_at_index(*head, idx - 1);
-	if (!cursor_before)
-	{
-		free(cursor_before);
+		free(cursor);
 		return (NULL);
 	}
 
-	temp->next = cursor_after;
-	cursor_before->next = temp;
+	temp->next = cursor->next;
+	cursor->next = temp;
 	return (temp);
 }
