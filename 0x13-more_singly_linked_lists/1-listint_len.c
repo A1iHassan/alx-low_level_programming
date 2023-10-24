ac291e92f160633cac7bc2fcff39fa95824a36a3
A1iHassan <ali012wkout@gmail.com>
2023-10-24 05:50:29 -0700

    -

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
+/**
+ * listint_len - counts list nodes
+ * @h: list head
+ * Return: nodes count
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *cursor = h;
 	int i = 0;
 
 	while (cursor)
+	{
 		i++;
+		cursor = cursor->next;
+	}
 	return (i);
 }
