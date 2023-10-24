88ffe1bf1e2e17d1c64c6004e8b5f2ede20598dc
A1iHassan <ali012wkout@gmail.com>
2023-10-24 15:31:38 -0700

    task 12 answer

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list without repetition
+ * @head: list head
+ * Return: nodes count
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *cursor = head;
+	int i = 0;
+
+	while (cursor)
+	{
+		printf("[%p] %d\n", (void *)cursor, cursor->n);
+		count++;
+
+		if (cursor <= cursor->next)
+		{
+			printf("-> [%p] %d\n", (void *)cursor->next, cursor->next->n);
+			exit(98);
+		}
+
+		cursor = cursor->next;
+	}
+
+	return (i);
+}
--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -1,83 +1,86 @@
 #include "lists.h"
 
 /**
- * listint_len - counts list nodes
- * @h: list head
- * Return: nodes count
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listadrs_s
+{
+	long int n;
+	struct listadrs_s *next;
+} listadrs_t;
+
+/**
+ * add_nodeint - inserts a new node at the begining of a list
+ * @head: list head
+ * @n: list content
+ * Return: new node's pointer
  */
 
-size_t listint_len(const listint_t *h)
+listadrs_t *add_nodeadrs(listadrs_t **head, long int n)
 {
-	const listint_t *cursor = h;
+	listadrs_t *temp;
+
+	temp = malloc(sizeof(listadrs_t));
+	if (!temp)
+	{
+		free(temp);
+		return (NULL);
+	}
+
+	temp->n = n;
+	temp->next = NULL;
+
+	if (!*head)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
+
+/**
+ * is_in - detects if the comtains the value
+ * @a: list head
+ * @b: desired value
+ * Return: success and faliure indecator
+ */
+
+int is_in(listadrs_t *a, long int b)
+{
+	listadrs_t *temp;
+
+	temp = a;
+	while (temp)
+	{
+		if (b == temp->n)
+			return (-1);
+		temp = temp->next;
+	}
+	return (1);
+}
+size_t print_listint_safe(const listint_t *head)
+{
+	listadrs_t *adrs = NULL;
+	const listint_t *cursor = head;
 	int i = 0;
 
 	while (cursor)
 	{
-		i++;
-		cursor = cursor->next;
+		if (is_in(adrs, (long int)(&cursor)))
+		{
+			add_nodeadrs(&adrs, (long int)(&cursor));
+			printf("[%p] %d\n", (void *)(&cursor), cursor->n);
+			i++;
+			cursor = cursor->next;
+		}
 	}
 	return (i);
-}
-
-/**
- * get_nodeint_at_index - searches a list by index
- * @head: list head
- * @index: position of desired node
- * Return: desired node
- */
-
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	unsigned int i = (unsigned int)listint_len(head);
-	int j;
-	listint_t *cursor;
-
-	if (!head || i < index)
-		return (NULL);
-
-	cursor = head;
-	for (j = index; j > 0; j--)
-		cursor = cursor->next;
-	return (cursor);
-}
-
-/**
- * swap_node - swaps to values in two nodes
- * @a: first node
- * @b: second node
- */
-
-void swap_node(listint_t *a, listint_t *b)
-{
-	int i;
-
-	i = a->n;
-	a->n = b->n;
-	b->n = i;
-}
-
-/**
- * reverse_listint - reverses the order of list nodes
- * @head: list head
- * Return: reversed list
- */
-
-listint_t *reverse_listint(listint_t **head)
-{
-	int i = 0, j = 0;
-
-	if (!*head)
-		return (NULL);
-	if (listint_len(*head) % 2 == 0)
-		i = listint_len(*head) / 2;
-	else
-		i = listint_len(*head) / 2 + 1;
-
-	while (i > 0)
-	{
-		swap_node(get_nodeint_at_index(*head, j),get_nodeint_at_index(*head, (unsigned int)listint_len(*head) - j - 1));
-		j++;
-		i--;
-	}
-	return (*head);
 }
