d89892c511ae1c061e9aa340aa7754f9cfdd5309
A1iHassan <ali012wkout@gmail.com>
2023-10-24 07:55:02 -0700

    -

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -32,7 +32,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	int j;
 	listint_t *cursor;
 
-	if (i < index - 1)
+	if (!head || i < index - 1)
 		return (NULL);
 
 	cursor = head;
