6d698fe8d026b0774c83a8c48a0262e40a6e89ae
A1iHassan <ali012wkout@gmail.com>
2023-10-30 18:35:56 -0700

    task 4 answer

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <elf.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define ELF_MAGIC 0x7f454c46
+
+/**
+  * print_elf_header - prints agiven ELF header
+  * @ehdr: pointer of type Elf64__Ehdr *
+  */
+
+void print_elf_header(const Elf64__Ehdr *ehdr)
+{
+	printf("Magic: 0x%08x\n", ehdr->e_ident[0]);
+	printf("Class: %d\n", ehdr->e_ident[4]);
+	printf("Data: %d\n", ehdr->e_ident[5]);
+	printf("Version: %d\n", ehdr->e_version);
+	printf("OS/ABI: %d\n", ehdr->e_ident[7]);
+	printf("ABI Version: %d\n", ehdr->e_ident[8]);
+	printf("Type: %d\n", ehdr->e_type);
+	printf("Entry point address: 0x%016lx\n", ehdr->e_entry);
+}
+
+/**
+ * main - program entry
+ * @argc: number of given arguments
+ * @argv: vector of argumets
+ * Return: a value of type int
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	ssize_t bytes_read;
+	Elf64__Ehdr ehdr;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error opening file: %s\n", argv[1]);
+		exit(98);
+	}
+
+	bytes_read = read(fd, &ehdr, sizeof(ehdr));
+	if (bytes_read != sizeof(ehdr))
+	{
+		dprintf(STDERR_FILENO, "Error reading ELF header\n");
+		exit(98);
+	}
+
+	if (ehdr.e_ident[0] != ELF_MAGIC)
+	{
+		dprintf(STDERR_FILENO, "Not an ELF file\n");
+		exit(98);
+	}
+
+	print_elf_header(&ehdr);
+
+	close(fd);
+
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,44 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct ELF - a data struct for all e_ instances neede
+ * @e_ident: first mamber
+ * @e_type: second member
+ * @e_machine: third member
+ * @e_version: forth member
+ * @e_entry: fifth member
+ * @e_phoff: sixth member
+ * @e_shoff: seventh member
+ * @e_flags: eightth member
+ * @e_ehsize: ninth member
+ * @e_phentsize: tenth member
+ * @e_phnum: eleventh member
+ * @e_shentsize: twelvth meber
+ * @e_shnum: therteenth member
+ * @e_shstrndx: forteenth member
+ * Description: ELF is a data struct neede for the program
+ */
+
+typedef struct ELF
+{
+	unsigned char e_ident[16];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	Elf64_Addr e_entry;
+	Elf64_Off e_phoff;
+	Elf64_Off e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} Elf64__Ehdr;
+
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
