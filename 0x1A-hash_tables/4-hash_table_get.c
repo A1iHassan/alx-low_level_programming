fb97b5b404929778bde4ed1f09cfea53e0672cb3
ALI <ali012wkout@gmail.com>
2023-12-22 23:37:53 +0200

    task 4 aNSWER

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,7 +21,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
-			return (temp->key);
+			return (temp->value);
 		temp = ht->array[i]->next;
 	}
 
