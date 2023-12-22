8ed981763bf5d048fbf0afe0eddd8b62684d8d0c
ALI <ali012wkout@gmail.com>
2023-12-22 21:38:11 +0200

    task 3 edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,30 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - inserts a new table cell
+ * @ht: hash table
+ * @key: key to be set or updated
+ * @value: value
+ * Return: 1 or 0
+*/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int x = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *temp = malloc(sizeof(hash_node_t));
 
 	if (key == NULL)
+	{
+		free(temp);
 		return (0);
+	}
+
+	if (strcmp(ht->array[x]->key, key) == 0)
+	{
+		ht->array[x]->value = strdup(value);
+		free(temp);
+		return (1);
+	}
 
 	if (!temp)
 	{
