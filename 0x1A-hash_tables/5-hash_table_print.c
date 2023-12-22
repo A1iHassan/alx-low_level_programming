751ac51887627a53c64738d10c0c9603cec22430
ALI <ali012wkout@gmail.com>
2023-12-23 00:27:33 +0200

    task 3 edit again

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,9 +2,28 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    if (ht == NULL || ht->array == NULL || ht->size == 0)
-    {
-        printf("{}\n");
-        return;
-    }
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			temp = ht->array[i];
+			while (temp != NULL)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+				if (temp != NULL)
+					printf(", ");
+			}
+		}
+	}
+	printf("}\n");
 }
