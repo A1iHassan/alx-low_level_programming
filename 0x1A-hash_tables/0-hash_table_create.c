210df0e4601c3fe1b327d857c3c0afd4e24f910e
ALI <ali012wkout@gmail.com>
2023-12-22 18:01:12 +0200

    task 0 edit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+	int i;
 
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	for (i = 0; table->array[i]; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
