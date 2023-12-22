f771bfcb46433b970dec1315a70b12e1685d4e8d
ALI <ali012wkout@gmail.com>
2023-12-22 21:28:49 +0200

    task 3 edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp = malloc(sizeof(hash_node_t));
 
 	if (key == NULL)
-		reurn (0);
+		return (0);
 
 	if (!temp)
 	{
