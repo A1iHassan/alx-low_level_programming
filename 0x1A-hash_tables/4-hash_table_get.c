902d29716395ae126b40ad41a08a821eafa7a826
ALI <ali012wkout@gmail.com>
2023-12-22 23:27:48 +0200

    task 4 aNSWER

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i;
 	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL || ht->array == NULL || ht->size == 0)
+    return (NULL);
+		
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
